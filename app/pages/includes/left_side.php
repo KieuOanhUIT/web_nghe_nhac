60407810f3d8cf5b526fcc8a98b44e13e8d0c09a
KieuOanhUIT <22521062@gm.uit.edu.vn>
2024-11-10 11:19:01 +0700

    update

---
+++ app/pages/includes/left_side.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/public/assets/css/header_main.css">
+    <link rel="stylesheet" href="/public/assets/css/main.css">
+    <link rel="stylesheet" href="/public/assets/css/listeningSpace.css">
+    <link rel="stylesheet" href="/public/assets/css/main_cpm.css">
+    <link rel="stylesheet" href="/public/assets/css/left_side.css">
+    <link rel="stylesheet" href="/public/assets/css/right_side.css">
+    <link rel="stylesheet" href="/public/assets/css/lyric.css">
+
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <title>Document</title>
+    <script src="https://code.iconify.design/iconify-icon/2.1.0/iconify-icon.min.js"></script>
+</head>
+<!-- leftBar -->
+<div class="leftBar">
+
+<div class="wrapper">
+    <div class="head">
+        <div class="head-1">
+            <div class="lib">
+                <iconify-icon icon="fluent:library-24-filled"></iconify-icon>
+                <p class="TV">Thư viện</p>
+            </div>
+            <div class="plus">
+                <iconify-icon icon="ic:sharp-plus"></iconify-icon>
+            </div>
+        </div>
+
+        <div class="head-2">
+            <div class="playlist">
+                Playlist
+            </div>
+            <div class="artist">
+                Nghệ sĩ
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="list">
+        <div class="box">
+            <div class="image">
+
+                <!--chèn hình ảnh-->
+                <img src="/public/assets/img/ha.svg" alt="">
+            </div>
+            <div class="song-artist">
+                <p class="BH"><!--chen ten bai hat-->Healing cực mạnh</p>
+                <p class="NS"><!--chen ten nghe si-->Playlist</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <!--chèn hình ảnh-->
+                <img src="/public/assets/img/ha1.svg" alt="">
+            </div>
+            <div class="song-artist">
+                <p class="BH"><!--chen ten bai hat-->Nhạc của Trang </p>
+                <p class="NS"><!--chen ten nghe si-->Nghệ sĩ</p>
+            </div>
+
+        </div>
+        <div class="box">
+            <div class="image">
+
+                <!--chèn hình ảnh-->
+                <img src="/public/assets/img/ha.svg" alt="">
+            </div>
+            <div class="song-artist">
+                <p class="BH"><!--chen ten bai hat-->Healing cực mạnh</p>
+                <p class="NS"><!--chen ten nghe si-->Playlist</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <!--chèn hình ảnh-->
+                <img src="/public/assets/img/ha1.svg" alt="">
+            </div>
+            <div class="song-artist">
+                <p class="BH"><!--chen ten bai hat-->Nhạc của Trang </p>
+                <p class="NS"><!--chen ten nghe si-->Nghệ sĩ</p>
+            </div>
+
+        </div>
+        <div class="box">
+            <div class="image">
+
+                <!--chèn hình ảnh-->
+                <img src="/public/assets/img/ha.svg" alt="">
+            </div>
+            <div class="song-artist">
+                <p class="BH"><!--chen ten bai hat-->Healing cực mạnh</p>
+                <p class="NS"><!--chen ten nghe si-->Playlist</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <!--chèn hình ảnh-->
+                <img src="/public/assets/img/ha1.svg" alt="">
+            </div>
+            <div class="song-artist">
+                <p class="BH"><!--chen ten bai hat-->Nhạc của Trang </p>
+                <p class="NS"><!--chen ten nghe si-->Nghệ sĩ</p>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+</div>
