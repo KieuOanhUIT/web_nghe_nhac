5c9c911ebad5e7aeca1fc82f28dc190b55708458
KieuOanhUIT <22521062@gm.uit.edu.vn>
2024-11-12 15:32:05 +0700

    update rightside

---
+++ app/pages/includes/right_side.php
@@ -0,0 +1,117 @@
+<!-- rightBar -->
+<div class="rightBar">
+                <div class="boxWrapper">
+                    <div class="exit">
+                        <iconify-icon icon="ic:outline-plus" id="plus-lbl"></iconify-icon>
+                        <iconify-icon icon="hugeicons:multiplication-sign" id="exit-lbl"></iconify-icon>
+                    </div>
+
+                    <div class="boxSongEvalute">
+                        <div class="boxSong">
+                            <div class="imageSong">
+                                <!--chèn hình ảnh-->
+                                <img src="/web_nghe_nhac/public/assets/img/ha3.svg" alt="" class="hinhanh">
+
+                            </div>
+
+                            <div class="nameSong">
+                                <div class="nameSong-Song">
+                                    I can do it with a broken heart
+                                </div>
+
+                                <div class="nameSong-Artist">
+                                    Taylor Swift
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="box-Evaluate">
+                            <div class="box-head">
+                                <div class="Evaluate-Lable">
+                                    Đánh giá
+                                </div>
+                                <div class="All">
+                                    Tất cả
+                                </div>
+                            </div>
+
+                            <div class="NameEvaluate">
+                                <div class="box-E">
+                                    <div class="Name">
+                                        <p>Nguyen Khoa Quan</p>
+                                    </div>
+                                    <div class="Eva">
+                                        <div class="Eva-Oval">
+                                            <span class="dot"></span>
+                                            <span class="dot"></span>
+                                            <span class="dot"></span>
+                                            <span class="dot"></span>
+                                            <span class="dot-empty"></span>
+                                        </div>
+                                        <div class="Eva-status">
+
+                                            <p>good</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="box-E">
+                                    <div class="Name">
+                                        <p>Nguyen Khoa Quan</p>
+                                    </div>
+                                    <div class="Eva">
+                                        <div class="Eva-Oval">
+                                            <span class="dot"></span>
+                                            <span class="dot"></span>
+                                            <span class="dot"></span>
+                                            <span class="dot"></span>
+                                            <span class="dot-empty"></span>
+                                        </div>
+                                        <div class="Eva-status">
+
+                                            <p>good</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="box-E">
+                                    <div class="Name">
+                                        <p>Nguyen Khoa Quan</p>
+                                    </div>
+                                    <div class="Eva">
+                                        <div class="Eva-Oval">
+                                            <span class="dot"></span>
+                                            <span class="dot"></span>
+                                            <span class="dot"></span>
+                                            <span class="dot"></span>
+                                            <span class="dot-empty"></span>
+                                        </div>
+                                        <div class="Eva-status">
+
+                                            <p>good</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="artist1">
+
+                            <div class="artist-image">
+                                <div class="textNgheSi">Nghệ sĩ</div>
+                                <img src="/web_nghe_nhac/public/assets/img/image.png" alt="" class="hinhanh1">
+                            </div>
+
+                            <div class="folow-artist">
+                                <div class="name">
+                                    <p>Taylor Swift</p>
+                                </div>
+                                <button id="follow">Theo dõi</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
