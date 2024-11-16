c1deb3aa6150f5b4d8216b351ae17d64bdede3a6
KieuOanhUIT <22521062@gm.uit.edu.vn>
2024-11-16 10:39:38 +0700

    update

--- app/core/init.php
+++ app/core/init.php
@@ -7,7 +7,3 @@ if (session_status() === PHP_SESSION_NONE) {
 // Đường dẫn tương đối tới các file cần thiết
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/functions.php';
-
-// Các file khác nếu cần
-// require_once __DIR__ . '/routes.php';
-// require_once __DIR__ . '/middleware.php';
