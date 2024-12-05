b752321ab7e96cace6755a81a709fd8e7172a3ef
KieuOanhUIT <22521062@gm.uit.edu.vn>
2024-12-05 09:46:47 +0700

    update admin left side

--- app/pages/admin/left_side.php
+++ app/pages/admin/left_side.php
@@ -1,19 +1,25 @@
 
 <li class="left-li">
+    <div class="menu-items">
         <a href="#" class="left-menu">Quản lý bài hát</a>
         <ul class="left-submenu">
             <li><a href="#"><iconify-icon icon="ic:round-add-circle"></iconify-icon>Thêm bài hát</a></li>
             <li><a href="#"><iconify-icon icon="solar:pen-bold"></iconify-icon>Cập nhật bài hát</a></li>
             <li><a href="#"><iconify-icon icon="ic:round-delete"></iconify-icon>Xóa bài hát</a></li>
         </ul>
+    </div>
+    <div class="menu-items">
         <a href="/web_nghe_nhac/app/pages/admin/manage_account.php" class="left-menu" id="account">Quản lý tài khoản</a>
         <ul class="left-submenu">
             <li id="figure-addAccount"><a href="#"><iconify-icon icon="ic:round-add-circle"></iconify-icon>Thêm tài khoản</a></li>
             <li id="figure-updateAccount"><a href="#"><iconify-icon icon="solar:pen-bold"></iconify-icon>Cập nhật tài khoản</a></li>
             <li id="figure-deleteAccount"><a href="#"><iconify-icon icon="ic:round-delete"></iconify-icon>Xóa tài khoản</a></li>
         </ul>
+    </div>
+    <div class="menu-items">
         <a href="/web_nghe_nhac/app/pages/admin/report.php" class="left-menu" id="report"><iconify-icon icon="carbon:report"></iconify-icon>Báo cáo</a>
-    </li>
+    </div>
+</li>
     <script src="/web_nghe_nhac/public/assets/script/admin_left_side.js"></script>
 
         <!-- Thêm tài khoản -->
